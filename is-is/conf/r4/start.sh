8670f585f0368a35af0443766fdedab3dcc2e25f
Alejandro Villaseca <avillaseca2001@gmail.com>
2024-10-14 11:51:59 +0200

    14/10

---
+++ is-is/conf/r4/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sysctl -w net.ipv6.conf.all.forwarding=1
+# enable Segment Routing for IPv6
+sysctl -w net.ipv6.conf.all.seg6_enabled=1
+for dev in $(ip -o -6 a | awk '{ print $2 }' | grep -v "lo")
+do
+   sysctl -w net.ipv6.conf."$dev".seg6_enabled=1
+done
