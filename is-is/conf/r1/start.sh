b0e84046288958a56d8174163c6c0ba8f90183c2
Alejandro Villaseca <avillaseca2001@gmail.com>
2024-10-16 13:14:16 +0200

    isis conf

--- is-is/conf/r1/start.sh
+++ is-is/conf/r1/start.sh
@@ -2,6 +2,7 @@
 sysctl -w net.ipv6.conf.all.forwarding=1
 # enable Segment Routing for IPv6
 sysctl -w net.ipv6.conf.all.seg6_enabled=1
+echo "no service integrated-vtysh-config" >> /etc/frr/vtysh.conf
 for dev in $(ip -o -6 a | awk '{ print $2 }' | grep -v "lo")
 do
    sysctl -w net.ipv6.conf."$dev".seg6_enabled=1
